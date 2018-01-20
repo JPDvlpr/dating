2251b1fc534e0be71b7bbb7b58652c8de1db4e73
Jhakon Pappoe <j.pappoe@hotmail.com>
2018-01-20 02:42:49 -0500

    add composer.json and index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jpapp
+ * Date: 1/14/2018
+ * Time: 10:14 PM
+ */
+
+//error reporting
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+require_once('vendor/autoload.php');
+
+$f3 = Base::instance();
+
+//set debug level
+$f3->set('DEBUG', 3);
+
+$f3->route('GET /',
+    function() {
+        $view = new View();
+        echo $view->render('pages/home.html');
+    }
+);
+
+//run fat free
+$f3->run();
+?>

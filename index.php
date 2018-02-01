6c783c52209b4849071b86b6548c47fb66e3df2d
Jhakon Pappoe <j.pappoe@hotmail.com>
2018-02-01 16:52:39 -0500

    add index and htaccess files

--- index.php
+++ index.php
@@ -19,9 +19,32 @@ $f3 = Base::instance();
 $f3->set('DEBUG', 3);
 
 $f3->route('GET /',
-    function() {
-        $view = new View();
-        echo $view->render('pages/home.html');
+    function () {
+        //load a template
+        $template = new Template();
+        echo $template->render('pages/home.html');
+
+    }
+);
+$f3->route('GET /personal',
+    function () {
+        //load a template
+        $template = new Template();
+        echo $template->render('pages/personal.php');
+    }
+);
+$f3->route('GET /profile',
+    function () {
+        //load a template
+        $template = new Template();
+        echo $template->render('pages/profile.php');
+    }
+);
+$f3->route('GET /interests',
+    function () {
+        //load a template
+        $template = new Template();
+        echo $template->render('pages/interests.html');
     }
 );
 
